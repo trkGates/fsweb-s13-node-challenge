fbe6401f6351b40f26c186e3fedb3c6b89ec2eed
Yusuf Çolak <yusuf21706@gmail.com>
2023-06-24 21:53:57 +0300

    projects_middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,33 @@
-// projects ara yazılımları buraya
+const projectModel = require("./projects-model");
+
+async function validateProjectId(req,res,next){
+    try {
+        const existProject = await projectModel.get(req.params.id);
+        if(!existProject){
+            res.status(404).json({message:"project not found"});
+        }else{
+            req.existProject = existProject;
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+function validateProjectPayload (req,res,next){
+    try {
+        const {name,description} = req.body;
+        if(!name || !description){
+            res.status(400).json({message:"missing fields"});
+        }else{
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    validateProjectId,
+    validateProjectPayload
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+server.use(express.json());
+const actionsRouter = require("./actions/actions-router");
+const projectRouter = require("./projects/projects-router");
 
-// Sunucunuzu yapılandırın
-// Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
-// Proje roterlarınızı /api/projects/projects-router.js içinde oluşturun
-// Bu dosyanın içinde `server.listen()` YAPMAYIN!
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectRouter);
 
 module.exports = server;
